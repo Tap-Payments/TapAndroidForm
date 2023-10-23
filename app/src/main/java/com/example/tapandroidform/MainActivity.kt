40d62f42dded050e212c8b48c692e6562587bfde
AhlaamK-tap <a.kazi@tap.company>
2023-10-23 12:52:07 +0300

    changed frame to framelayout

--- app/src/main/java/com/example/tapandroidform/MainActivity.kt
+++ app/src/main/java/com/example/tapandroidform/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.tapandroidform
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.preference.Preference
+import com.chillibits.simplesettings.core.SimpleSettings
+import com.chillibits.simplesettings.core.SimpleSettingsConfig
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),SimpleSettingsConfig.PreferenceCallback  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val configuration = SimpleSettingsConfig.Builder()
+            .setActivityTitle("Configuration")
+            .setPreferenceCallback(this)
+            .displayHomeAsUpEnabled(false)
+            .build()
+        SimpleSettings(this, configuration).show(R.xml.preferences)
+    }
+    override fun onPreferenceClick(context: Context, key: String): Preference.OnPreferenceClickListener? {
+        return when(key) {
+            "dialog_preference" -> Preference.OnPreferenceClickListener {
+                val intent = Intent(this@MainActivity, SecondActivity::class.java)
+                finish()
+                startActivity(intent)
+
+                true
+            }
+
+            else -> super.onPreferenceClick(context, key)
+        }
     }
 }
